3cdcde8bb5086712423ea761cba52301726e3832
ozkarta <ozbegi1@gmail.com>
2018-02-23 23:34:57 +0400

    Route is fixed

--- src/app/components/shared/routes.ts
+++ src/app/components/shared/routes.ts
@@ -1,16 +1,18 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {MessengerComponent} from './messenger/messenger.component';
+import {UserAuthGuard} from '../user/user-auth.guard';
 
 export const routes: Routes = [
-	{
-		path: 'messenger',
-		component: MessengerComponent
-	}
+  {
+    path: 'messenger',
+    component: MessengerComponent,
+    canActivate: [UserAuthGuard]
+  }
 ];
 
 @NgModule({
-	imports: [ RouterModule.forRoot(routes, {enableTracing: true}) ],
-	exports: [ RouterModule ]
+  imports: [ RouterModule.forRoot(routes, {enableTracing: true}) ],
+  exports: [ RouterModule ]
 })
 export class SharedRoutingModule {}
